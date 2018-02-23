f465f6b8a744d2f4e97d9bb406f82413f46336ad
Asain Kujovic <asainnp@gmail.com>
2018-02-23 05:32:14 +0100

    trap EXIT in revtunnel

--- revtunnel.sh
+++ revtunnel.sh
@@ -1,32 +1,46 @@
 #!/usr/bin/env bash
-
 # this script is not called directly, it is called over revtunnel.sh instead for su user-switch
 
-cd $(dirname $0) ;   . ./config.sh # 5 variables: runninguser, fullsrvlogin, tunnelport, fulldstlogin, dsthostname.
-loggingfile=/tmp/lastrevtunnel.log # for main loop logging
+trap 'jobs -p | xargs -r kill; killloop' EXIT                # so that 'systemctl stop' kills subprocess too.
 
-loginfull2array() { echo $1 | sed 's/^\(.*\)@\(.*\):\(.*\)$/\1 \2 \3/'; }
+########## load vars from config.sh: #############
+cd $(dirname $0) ;   . ./config.sh  # 5 vars: runninguser, fullsrvlogin, tunnelport, fulldstlogin, dsthostname.
+loggingfile=/tmp/lastrevtunnel.log  # for main loop logging
+scrbasename=$(basename $0)
 
-sarr=( $(loginfull2array $fullsrvlogin) ) ; srvlogname=${sarr[0]} ; srvip=${sarr[1]} ; srvsshport=${sarr[2]}
-darr=( $(loginfull2array $fulldstlogin) ) ; dstlogname=${darr[0]} ; dstip=${darr[1]} ; dstsshport=${darr[2]}
-
-id -u $runninguser >/dev/null || { echo "err: user '$runninguser' does not exists on current comp."; exit 1; }
+########## ensure correct running user: ##########
+id -u "$runninguser" >/dev/null || { echo "err: user '$runninguser' does not exists."; exit 1; }
 case "$(whoami)" in
    $runninguser) ;; # ok, continue script
            root) su $runninguser -c "$0 $*" ; exit $? ;;
-              *) echo "err: you should be user: '$runninguser' (from config.sh) or root in order to run revtunnel."
+              *) echo "err: you should be user: '$runninguser' (from config.sh) or 'root' in order to run revtunnel."
                  exit 1 ;;
 esac
-
-starttunnel() { ssh -o 'BatchMode yes' -o 'ExitOnForwardFailure yes' -fNTR $srvip:$tunnelport:$dstip:$dstsshport -p$srvsshport $srvlogname@$srvip; }
-killtunnel()  { pkill -f "ssh .* -fNTR $srvip:$tunnelport"; }
+########## functions: ############################
+loginfull2array() { echo $1 | sed 's/^\(.*\)@\(.*\):\(.*\)$/\1 \2 \3/'; }
+starttunnel() { ssh -o 'BatchMode yes' -o 'ExitOnForwardFailure yes' -fNT \
+	            -R $srvip:$tunnelport:$dstip:$dstsshport -p$srvsshport $srvlogname@$srvip; }
+killtunnel()  { pkill -f "ssh .* -R $srvip:$tunnelport"; }
 killremote()  { ssh -p$srvsshport $srvlogname@$srvip 'pkill -u $srvlogname sshd'; }
 checktunnel() { [ "x$dsthostname" = "x$(ssh -p $tunnelport $srvip hostname)" ] && return 0 || return 1; }
 restartall()  { killremote; killtunnel; starttunnel; }
-
+mainwhileloop() 
+{  printf "$(date): starting $scrbasename" > $loggingfile
+   starttunnel ; dotsok=0 ; dotser=0
+   while true; do
+      if checktunnel
+         then dotser=0 ; [ $((++dotsok%60)) -eq 1 ] && printf "\n$(date), tunnel is ok, ok30s: "
+         else dotsok=0 ; [ $((++dotser%60)) -eq 1 ] && printf "\n$(date), tunnel error, er30s: "
+              restartall
+      fi
+      printf "." ; sleep 30 
+   done >> $loggingfile
+}
+killloop() { pkill -f "$(basename $scrbasename) startloop" ; killtunnel; }
 checksshsimplenohkey(){ ssh -o 'BatchMode yes' -o 'StrictHostKeyChecking no' -p $3 $1@$2 hostname; return $?; }
 checksshsimpleonce()  { ssh -o 'BatchMode yes' -p $3 $1@$2 hostname; return $?; }
-checksshsimple()      { sshuser=$1 ; sshserver=$2 ; sshport=$3 ; sshusp="$1@$2:$3"
+checksshsimple()
+{  sshuser=$1 ; sshserver=$2 ; sshport=$3 ; sshusp="$1@$2:$3"
    if checksshsimpleonce $@; then return 0 # ok
    else if checksshsimplenohkey $@; then   # no need for ssh-keygen -R $sshserver:$sshport beause...
            return 0  # ...'StrictHostKeyhecking no' will add current and remove prev key if exists
@@ -42,19 +56,15 @@ checksshsimple()      { sshuser=$1 ; sshserver=$2 ; sshport=$3 ; sshusp="$1@$2:$
         fi
    fi 
 }
+########## main switch-case: ####################
+
+sarr=( $(loginfull2array $fullsrvlogin) ) ; srvlogname=${sarr[0]} ; srvip=${sarr[1]} ; srvsshport=${sarr[2]}
+darr=( $(loginfull2array $fulldstlogin) ) ; dstlogname=${darr[0]} ; dstip=${darr[1]} ; dstsshport=${darr[2]}
 
 case "$1" in
-      starttunnel) starttunnel ;;
-       killtunnel) killtunnel  ;;
-        startloop) printf "$(date): starting $0" > $loggingfile ; dotsok=0 ; dotser=0
-                   while true; do
-                      if checktunnel; then dotser=0 ; [ $((++dotsok%60)) -eq 1 ] && printf "\n$(date), tunnel is ok, ok30s: "
-                                      else dotsok=0 ; [ $((++dotser%60)) -eq 1 ] && printf "\n$(date), tunnel error, er30s: "
-                                           ./revtunnel.sh restart; fi
-                      printf "." ; sleep 30 
-                   done >> $loggingfile ;;
-          restart) restartall  ;;
-      checktunnel) if checktunnel; then echo ...ok; else exit 1; fi ;;
+        startloop) mainwhileloop ;;
+         killloop) killloop ;;
+   ############### checkings for Makefile: ###########################
          checkssh) if checksshsimple $srvlogname $srvip $srvsshport; then echo ...ok; else exit 1; fi ;;
       checksshfwd) killtunnel
                    if starttunnel; then killtunnel ; echo ...ok; exit 0;
@@ -71,4 +81,6 @@ case "$1" in
                    killtunnel; exit $err ;;
                 *) echo "unknown param1 '$1' for revtunnel script."
 esac
+
+########## eof. #################################
 
