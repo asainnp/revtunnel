e6f10319cca47a7a52329aad2bd52c49f49b1656
Asain Kujovic <asain.kujovic@bstelecom.ba>
2018-02-26 00:04:00 +0100

    shrink to 77 lines

--- revtunnel.sh
+++ revtunnel.sh
@@ -37,12 +37,10 @@ checksshsimple()
    if checksshonce $1 $2 $3 BS; then return 0; fi # try ssh with non-strict key checking
    ssh-keygen -R "[$sshserver]:$sshport"          # try removing key, for (non-strict checking will add new value automatically)
    if checksshonce $1 $2 $3 BS; then return 0; fi    
-   echo    "err: passwordless ssh to '$sshusp' not working"
-   read -p "     do you want to try ssh-copy-id to $sshusp as $(whoami)? " varreply 
-   case "$varreply" in [Yy]*)
-      ssh-copy-id -p$sshport $sshuser@$sshserver # try ssh-copy-id
-      if checksshonce $1 $2 $3 B; then return 0; fi ;;
-   esac
+   read -p "err:\t passwordless ssh to '$sshusp' not working\n\tdo you want to try ssh-copy-id to $sshusp as $(whoami)? " ans
+   echo $answ | grep -iq '^y' || exit 1           # exit if user didn't response with y/Y...
+   ssh-copy-id -p$sshport $sshuser@$sshserver     # try ssh-copy-id
+   if checksshonce $1 $2 $3 B; then return 0; fi 
    return 1
 }
 mylogrotate() { fname="$1"; for i in {7..0}; do [ -e $fname.$i ] && mv $fname.$i $fname.$((i+1)); done
